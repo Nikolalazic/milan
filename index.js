329b4dbd4db1bc0218ba79ecc61d4b6da5456d81
Nikolalazic <nikolalazic193@gmail.com>
2019-05-03 11:59:33 +0200

    push

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-flks
+
+
+
+// strings
+// String je text
+string1 = "niko";
+string2 = "2";
+string3 = "MIlan23";
+
+// Intiger
+// Brojevi
+
+broj = 2;
+
+// Array{NIZ}
+
+array = [
+	{
+		mina: "minic",
+		milan: "Jankovic",
+	},
+	"nikola",
+	5,
+	[
+		{
+			milan2: "janko",
+		},
+		"sarma",
+	],
+	];
+
+	for(i = 0; i < array.length; i++){
+		if(typeof array[i] === "string"){
+			console.log(array[i]);
+		}
+		console.log('nema stringa');
+	}
+
+
